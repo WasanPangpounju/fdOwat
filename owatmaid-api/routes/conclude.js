fb1f0be67d8e2c41f0ca822b1ad80d1393635b0b
WasanPangpounju <friendlydev.net@gmail.com>
2025-01-27 12:21:59 +0700

    update up salary

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -67,6 +67,8 @@ router.post('/autocreate', async (req, res) => {
 
 const workplaceListTmp = [];
 const upsalary = 0;
+const upSalary_year = '';
+const upSalary_month  = '';
 
   try {
 
@@ -185,9 +187,9 @@ console.log(workRateChange );
 const date = new Date(workRateChange);
 
 // Get the year
-const upSalary_year = date.getFullYear(); // Use getFullYear() for local time
+upSalary_year = date.getFullYear(); // Use getFullYear() for local time
 // Get the month (0-based index, so add 1 for the correct month)
-const upSalary_month = date.getMonth() + 1; // Use getMonth() for local time
+upSalary_month = date.getMonth() + 1; // Use getMonth() for local time
 //check up Salary with month and year
 if((prevMonth  == upSalary_month ) && (year1  == upSalary_year ) ) {
   salary  = await parseFloat(salary)   + parseFloat(upsalary  || '0');
