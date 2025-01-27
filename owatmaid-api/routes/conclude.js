eb2484e5a426e767b3db67b0b4639bfb153833a7
WasanPangpounju <friendlydev.net@gmail.com>
2025-01-27 12:17:01 +0700

    update up salary

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -790,7 +790,7 @@ const         wpDataCalculator1 = await {
 //check up Salary with month and year
 // if((month == upSalary_month ) && (year == upSalary_year ) ) {
   salary  = await parseFloat(salary)   + parseFloat(upsalary  || '0');
-  tmpSalary = await parseFloat(tmpSalary)  + salary   + parseFloat(upsalary  || '0');
+  temSalary = await parseFloat(temSalary )  + salary   + parseFloat(upsalary  || '0');
 // }
 
       // console.log('wGroup X ' + JSON.stringify(wGroup    ,2,null))
