b56652b12929e3faa9201b46f5ab28b219af6bea
WasanPangpounju <friendlydev.net@gmail.com>
2025-01-27 13:07:00 +0700

    update up salary

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -66,9 +66,9 @@ router.post('/autocreate', async (req, res) => {
   sumWorkRateOt = 0;
 
 const workplaceListTmp = [];
-const upsalary = 0;
-const upSalary_year = '';
-const upSalary_month  = '';
+// const upsalary = 0;
+// const upSalary_year = '';
+// const upSalary_month  = '';
 //check up Salary and up Salary
 //check data with workplace 
 const wsSearch  = {
@@ -79,16 +79,16 @@ const wsSearch  = {
 try {
   const empWpResponse = await axios.post(`${sURL}/workplace/getupsalary`, wsSearch );
   if (empWpResponse.data.workplaces) {
-    upsalary = await empWpResponse?.data?.workplaces?.[0]?.addWorkRate || 0;
+    const upsalary = await empWpResponse?.data?.workplaces?.[0]?.addWorkRate || 0;
     const workRateChange = await empWpResponse?.data?.workplaces?.[0]?.workRateChange || 0;
 console.log(workRateChange );
 // Convert the string to a Date object
 const date = new Date(workRateChange);
 
 // Get the year
-upSalary_year = date.getFullYear(); // Use getFullYear() for local time
+const upSalary_year = date.getFullYear(); // Use getFullYear() for local time
 // Get the month (0-based index, so add 1 for the correct month)
-upSalary_month = date.getMonth() + 1; // Use getMonth() for local time
+const upSalary_month = date.getMonth() + 1; // Use getMonth() for local time
 //check up Salary with month and year
 // if((prevMonth  == upSalary_month ) && (year1  == upSalary_year ) ) {
   // salary  = await parseFloat(salary)   + parseFloat(upsalary  || '0');
