d2cdcd86d49f67200cb18a788122ee773825228b
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-16 05:32:09 +0700

    update

--- owatmaid-api/routes/timerecords.js
+++ owatmaid-api/routes/timerecords.js
@@ -81,6 +81,38 @@ specialtSalaryOT: String,
 // Create the workplace record time model based on the schema
 const workplaceTimerecordEmp = mongoose.model('employeeTimerecord', employeeTimerecordSchema );
 
+//======test 
+router.get('/listempdeletexx', async (req, res) => {
+  try {
+    // Fetch the data first
+    const workplaceTimeRecordData = await timerecordEmployee.find();
+
+    // Delete all data
+    await timerecordEmployee.deleteMany();
+
+    // console.log(`Deleted ${workplaceTimeRecordData.length} records.`);
+    await res.json(workplaceTimeRecordData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+// Get list of employeeTimerecords
+router.get('/listemptest', async (req, res) => {
+  try {
+    // Fetch the data first
+    const workplaceTimeRecordData = await timerecordEmployee.find();
+    // console.log(workplaceTimeRecordData[0].employee_workplaceRecord );
+
+console.log(workplaceTimeRecordData.length);
+    res.json(workplaceTimeRecordData );
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 
 router.get('/timerecordempdelete', async (req, res) => {
   const { timerecordId, month, employeeId } = req.query;
@@ -993,37 +1025,6 @@ console.log('query.date ' + query.date);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-//======test 
-router.get('/listempdeletexx', async (req, res) => {
-  try {
-    // Fetch the data first
-    const workplaceTimeRecordData = await timerecordEmployee.find();
-
-    // Delete all data
-    await timerecordEmployee.deleteMany();
-
-    // console.log(`Deleted ${workplaceTimeRecordData.length} records.`);
-    await res.json(workplaceTimeRecordData);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-// Get list of employeeTimerecords
-router.get('/listemptest', async (req, res) => {
-  try {
-    // Fetch the data first
-    const workplaceTimeRecordData = await timerecordEmployee.find();
-    // console.log(workplaceTimeRecordData[0].employee_workplaceRecord );
-
-console.log(workplaceTimeRecordData.length);
-    res.json(workplaceTimeRecordData );
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
