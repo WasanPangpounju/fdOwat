4c1f94e927177cd4c759716727168fb49afebfff
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-24 11:12:00 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1786,7 +1786,7 @@ const calculateCashValues = async (employee_record) => {
       // Fetch workplace data
       const workplaceData = await Workplace.findOne(query);
 
-      console.log("Workplace Data:", JSON.stringify(workplaceData, null, 2));
+      console.log("Workplace Data:", JSON.stringify(workplaceData.workRate, null, 2));
 
       return {
         ...record.toObject(), // Convert Mongoose document to plain object
