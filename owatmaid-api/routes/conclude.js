16c0fca9acb1a66d508783afef89dd712649aa2b
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-25 07:19:01 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1782,7 +1782,8 @@ const calculateCashValues = (employee_record) => {
 
   return employee_record.map(record => {
     const query = {};
-
+    const recordData = record.toObject ? record.toObject() : record;
+    console.log(JSON.stringify(recordData ,null,2))
     if (record.workplaceId !== '') {
         query.workplaceId = record.workplaceId;
     }
