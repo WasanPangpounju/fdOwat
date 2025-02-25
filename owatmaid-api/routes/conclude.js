6b992a105ada048edda5acc3a5121bfa5515b7fa
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-25 07:19:51 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1790,8 +1790,8 @@ const calculateCashValues = (employee_record) => {
 
     const workplaceData  = Workplace.find(query.workplaces );
 
-    console.log(workplaceData);
-console.log("record.workplaceId ", record.workplaceId)
+    // console.log(workplaceData);
+// console.log("record.workplaceId ", record.workplaceId)
 
     return {
       ...record.toObject(), // Convert Mongoose document to plain object
