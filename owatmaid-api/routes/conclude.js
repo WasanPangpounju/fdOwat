54b46b1b271185c3c52f470e5647c5c83a132f6f
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-27 04:42:00 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1851,11 +1851,11 @@ return dataCal;
 // Function to calculate cash values
 const calculateCashValues = async (employee_record, month, year ) => {
 
- return employee_record.map(async record => {
+ return employee_record.map(record => {
 // console.log(record.workplaceId|| 0);
 // console.log(record.wGroup || '');
 // console.log(record.date || '');
-let dataRate = await checkDayRate(record.workplaceId, record.wGroup , new Date(year, month -1, record.date ));
+let dataRate = checkDayRate(record.workplaceId, record.wGroup , new Date(year, month -1, record.date ));
 
   return {
       ...record.toObject(), // Convert Mongoose document to plain object
