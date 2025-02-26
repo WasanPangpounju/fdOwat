fac92a37b7a9978c2aef808527d14a1f78b5f98f
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-27 02:59:10 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1824,7 +1824,7 @@ dataCal.holidayOT = await workplaces[0].holidayOT || 0;
 
 //check day type
 for(const workTimeDay of workplaces[0].workTimeDay) {
-  let check = checkdayType(workTimeDay.startDay, workTimeDay.endDay , date.getDay)();
+  let check = checkdayType(workTimeDay.startDay, workTimeDay.endDay , date.getDay());
   if(check) {
     // console.log(workTimeDay.workOrStop )
 // console.log("data " ,workTimeDay.startDay, workTimeDay.endDay , date.getDay() );
