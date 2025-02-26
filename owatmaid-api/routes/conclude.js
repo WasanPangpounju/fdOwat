bc82f4facda5d63fe93a111ceb0a0e5ece210372
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-27 04:43:06 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1830,7 +1830,7 @@ dataCal.holiday = await workplaces?.[0]?.holiday || 0;
 dataCal.holidayOT = await workplaces?.[0]?.holidayOT || 0;
 
 //check day type
-for(const workTimeDay of workplaces?.[0]?.workTimeDay) {
+for(const workTimeDay of workplaces[0].workTimeDay) {
   let check = checkdayType(workTimeDay.startDay, workTimeDay.endDay , date.getDay());
   if(check === true) {
     console.log(workTimeDay.workOrStop )
