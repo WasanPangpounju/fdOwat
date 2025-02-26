51e220f12a6f0137260e9bbd3e1fb1860e970902
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-27 04:53:06 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1850,7 +1850,7 @@ return dataCal;
 }
 
 // Function to calculate cash values
-const calculateCashValues = async (employee_record, month, year ) => {
+const calculateCashValues = (employee_record, month, year ) => {
 
  return employee_record.map(record => {
 // console.log(record.workplaceId|| 0);
