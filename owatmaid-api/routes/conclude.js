f5c4ffe0be64953d8f6438a344ca22ffcdeba27d
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-27 21:06:03 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1865,7 +1865,7 @@ let cashBeforeOt = await (record.beforeTotalOtTime || 0) * parseFloat(dataRate.w
 let cashWork = await (record.totalTime || 0) * parseFloat(dataRate.workRate || '0');
 let cashOt = await (record.totalOtTime || 0) * parseFloat(dataRate.workRateOT || '0');
 
-  return {
+  return await {
       ...record.toObject(), // Convert Mongoose document to plain object
 
       cashBeforeOt: cashBeforeOt ,
