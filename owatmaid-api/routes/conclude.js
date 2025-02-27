7432b2ce3ccaa65bf1cdf47e05bae6f751c7cf94
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-28 06:20:28 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1851,40 +1851,35 @@ return dataCal;
 
 }
 
+
 const calculateCashValues = async (employee_record, month, year) => {
   return Promise.all(
     employee_record.map(async (record) => {
       const dataRate = await checkDayRate(record.workplaceId, record.wGroup, new Date(year, month - 1, record.date));
+      let cashBeforeOt = 0;
+      let cashWork = 0;
+      let cashOt = 0;
+      let dayType = '';
 
       //check dayType
       // if(dataRate?.dayType || '' !== '' ) {
         if (dataRate?.dayType !== '') {
       // if(dataRate?.dayType || '' === 'stop') {
         if (dataRate?.dayType === 'stop') {
-        // let cashBeforeOt = ((record.beforeTotalOtTime || 0) * (parseFloat(dataRate?.dayoffRateOT || '0') * parseFloat(dataRate?.workRate || '0') || 0)) || '';
-        let cashBeforeOt = 0;
-
+        let cashBeforeOt = ((record.beforeTotalOtTime || 0) * (parseFloat(dataRate?.dayoffRateOT || '0') * parseFloat(dataRate?.workRate || '0') || 0)) || '';
         let cashWork = (record.totalTime || 0) * (parseFloat(dataRate?.workRate || '0')* parseFloat(dataRate?.dayoffRateHour || '0')) || '';
         let cashOt = (record.totalOtTime || 0) * (parseFloat(dataRate?.dayoffRateOT || '0') * parseFloat(dataRate?.workRate || '0')) || '';
         let dayType = dataRate?.dayType || '';
   
     } else {
 
-      // let cashBeforeOt = (record.beforeTotalOtTime || 0) * (parseFloat(dataRate?.workRateOT || '0') * parseFloat(dataRate?.workRate || '0'));
-      let cashBeforeOt  = 0;
+      let cashBeforeOt = (record.beforeTotalOtTime || 0) * (parseFloat(dataRate?.workRateOT || '0') * parseFloat(dataRate?.workRate || '0'));
       let cashWork = (record.totalTime || 0) * parseFloat(dataRate?.workRate || '0');
       let cashOt = (record.totalOtTime || 0) * (parseFloat(dataRate?.workRateOT || '0') * parseFloat(dataRate?.workRate || '0'));
       let dayType = dataRate?.dayType || '';
     }
-
-  } else {
-          let cashBeforeOt = (record.beforeTotalOtTime || 0) * (parseFloat(dataRate?.workRateOT || '0') * parseFloat(dataRate?.workRate || '0'));
-          let cashWork = (record.totalTime || 0) * parseFloat(dataRate?.workRate || '0');
-          let cashOt = (record.totalOtTime || 0) * (parseFloat(dataRate?.workRateOT || '0') * parseFloat(dataRate?.workRate || '0'));
-          // let dayType = dataRate?.dayType || '';
-        }
     
-  // }
+  }
 
       return {
         ...record,
