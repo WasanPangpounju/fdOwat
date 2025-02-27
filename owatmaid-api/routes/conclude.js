139a8317d4b2dc0e959b2f2c8a84a9d6a9cd0d7f
WasanPangpounju <friendlydev.net@gmail.com>
2025-02-28 06:11:04 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1855,7 +1855,7 @@ const calculateCashValues = async (employee_record, month, year) => {
   return Promise.all(
     employee_record.map(async (record) => {
       const dataRate = await checkDayRate(record.workplaceId, record.wGroup, new Date(year, month - 1, record.date));
-console.log(dataRate?.dayType );
+
       //check dayType
       // if(dataRate?.dayType || '' !== '' ) {
         if (dataRate?.dayType !== '') {
@@ -1877,6 +1877,13 @@ console.log(dataRate?.dayType );
       let dayType = dataRate?.dayType || '';
     }
 
+  } else {
+          let cashBeforeOt = (record.beforeTotalOtTime || 0) * (parseFloat(dataRate?.workRateOT || '0') * parseFloat(dataRate?.workRate || '0'));
+          let cashWork = (record.totalTime || 0) * parseFloat(dataRate?.workRate || '0');
+          let cashOt = (record.totalOtTime || 0) * (parseFloat(dataRate?.workRateOT || '0') * parseFloat(dataRate?.workRate || '0'));
+          let dayType = dataRate?.dayType || '';
+        }
+    
   }
 
       return {
