e783ec0577454113cc2f0835147babfbe6a66f0f
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-05 04:45:32 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1890,6 +1890,8 @@ const getEmployeeProfile = async (employeeId) => {
 const calculateCashValues = async (employeeId, employee_record, month, year) => {
   const employeeProfile = await getEmployeeProfile(employeeId);
 const salaryTmp = await parseFloat(employeeProfile[0].salary || '0') || 0;
+const salary = 0;
+
 if(parseFloat(salary || '0')  > 1660) {
   salary = await parseFloat(salaryTmp || '0') / 30;
 }
