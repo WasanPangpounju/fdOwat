7a94b9bc5f55497e6248c894edbe55825e43ba69
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-05 04:09:00 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1897,8 +1897,8 @@ month = 12;
 }
       const dataRate = await checkDayRate(record.workplaceId, record.wGroup, new Date(year, month - 1, record.date));
 
-console.log(record.date + employeeProfile[0].idCard );
-
+console.log(record.date );
+console.log(JSON.stringify(employeeProfile,null,2))
       let cashBeforeOt = 0;
       let cashWork = 0;
       let cashOt = 0;
