0bed6b86f29fab985f4b4a8aa7b4d6ec0292e6d5
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-13 06:50:07 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1914,7 +1914,7 @@ const workplaceId = employeeProfile[0].workplace === "10105" ? "10105" : record.
 
 // console.log(record.date );
 // console.log(employeeId + JSON.stringify(employeeProfile[0].salary,null,2))
-console.log('add salary' + JSON.stringify(addSalary,null,2) );
+// console.log('add salary' + JSON.stringify(addSalary,null,2) );
 
 
       let cashBeforeOt = 0;
@@ -1973,7 +1973,7 @@ addSalaryDaily = addSalary;
     }
     
   }
-
+console.log(addSalaryDaily );
       return {
         ...record,
         cashBeforeOt,
