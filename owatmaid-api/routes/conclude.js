fac4e74338afa6f04b7c1d61c31821f9e20051d9
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-13 06:54:29 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1973,7 +1973,7 @@ addSalaryDaily = addSalary;
     }
     
   }
-console.log(addSalaryDaily );
+
       return {
         ...record,
         cashBeforeOt,
@@ -2074,7 +2074,7 @@ router.post('/searchtimerecordemployee', async (req, res) => {
         console.error("❌ Error in calculateCashValues:", error);
       }
     }
-    
+    console.log(result .length)
     await res.status(200).json({ result });
 
   } catch (error) {
