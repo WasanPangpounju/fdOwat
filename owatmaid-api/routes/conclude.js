10df4f438d08eb498c2a41be342d8c1da94d42bd
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-13 07:09:50 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1970,7 +1970,7 @@ let addSalaryDaily = [];
 //        cashWorkMul = 1;
 //        cashOtMul = await dataRate?.workRateOT || 0;
 addSalaryDaily = await employeeProfile[0].addSalary;
-
+console.log(employeeProfile[0].addSalary);
     }
     
   }
