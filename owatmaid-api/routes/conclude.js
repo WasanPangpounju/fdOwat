51e6e5561466254d49bec4e25fe566094392c503
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-13 08:20:58 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1969,8 +1969,10 @@ let addSalaryDaily = [];
        cashBeforeOtMul = await dataRate?.workRateOT ||  0;
        cashWorkMul = 1;
        cashOtMul = await dataRate?.workRateOT || 0;
-       addSalaryDaily = [...(employeeProfile[0].addSalary || [{'x': 'y'} , {'a': 'b'}])];
-console.log("addsalary " + JSON.stringify( employeeProfile[0].addSalary ,null,2));
+      //  addSalaryDaily = [...(employeeProfile[0].addSalary || [])];
+      addSalaryDaily = [...(employeeProfile[0].addSalary || []).filter(salary => salary.roundOfSalary === "daily")];
+
+// console.log("addsalary " + JSON.stringify( employeeProfile[0].addSalary ,null,2));
     }
     
   }
