7ff4a322a0d1f6aaf470085f647b3b88bac35407
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-21 04:04:49 +0700

    update accounting sum addSalaryDaily

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4482,6 +4482,7 @@ sumCashWorkMul[record?.cashOtMul ] += parseFloat(record?.cashBeforeOt || '0');
   console.log('dayOffCount : ' + dayOffCount);
   console.log('specialDayOff  : ' + specialDayOff );
 console.log(JSON.stringify(addSalaryDailyList,null,2));
+
 if(salaryMonth !== 0) {
   dayWorkCount = 30;
   sumCashWork  = salaryMonth;  
