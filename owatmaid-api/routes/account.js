d1644649c1e1fbf56c57ed03ab93dcff37c0c69b
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-21 06:44:22 +0700

    update accounting sum addSalaryDaily

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4497,6 +4497,11 @@ monthlySalaries = addSalary.filter(salary => salary.roundOfSalary === 'monthly')
     ...addSalaryDailyList,
     ...monthlySalaries
   ];
+
+  addSalaryList.forEach(element => {
+   console.log(element.SpSalary );
+  });
+
 // console.log(JSON.stringify(addSalaryList[0].SpSalary ,null,2));
 
 if(salaryMonth !== 0) {
