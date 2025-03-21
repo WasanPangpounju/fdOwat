d5d07f80c2e06f828c976ef9f0dff2ca5fce5488
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-21 11:12:37 +0700

    update accounting sum addSalaryDaily

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4463,12 +4463,12 @@ sumCashWorkMul[record?.cashOtMul ] += parseFloat(record?.cashBeforeOt || '0');
       if (existingItem) {
         // existingItem.SpSalary = parseFloat(existingItem.SpSalary  || '0') + parseFloat(amount || '0');
       } else {
-        addSalaryDailyList.push(salaryItem);
-        //   {
-        //   ...salaryItem,
-        //   id: cleanSalaryItemId,
-        //   SpSalary: amount,
-        // });
+        addSalaryDailyList.push(
+          {
+          ...salaryItem,
+          id: cleanSalaryItemId,
+          SpSalary: amount,
+        });
 
       }
     });
