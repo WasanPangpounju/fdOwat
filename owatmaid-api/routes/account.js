97648a81dbc5ccc68f6928c2679ddaa030c217e2
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-25 14:34:40 +0700

    edit account

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4461,7 +4461,7 @@ sumCashWorkMul[record?.cashOtMul ] += parseFloat(record?.cashBeforeOt || '0');
   
       if (existingItem) {
         // clearly ensure numeric addition
-        existingItem.SpSalary = parseFloat((existingItem.SpSalary + amount).toFixed(2));
+        // existingItem.SpSalary = parseFloat((existingItem.SpSalary + amount).toFixed(2));
       } else {
         addSalaryDailyList.push({
           ...salaryItem,
