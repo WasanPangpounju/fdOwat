98c141f0ab411294c972c4a762b8e145a84b2041
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-25 14:56:57 +0700

    edit account

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4469,7 +4469,7 @@ sumCashWorkMul[record?.cashOtMul ] += parseFloat(record?.cashBeforeOt || '0');
         addSalaryList .push({
           ...salaryItem,
           id: cleanSalaryItemId,
-          SpSalary: salaryItem.SpSalary,
+          SpSalary: 0,
         });
       }      
     });
