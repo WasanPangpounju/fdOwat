f4917d8025950647bc9faeaf4cfcefee80839ebf
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-29 15:27:16 +0700

    update account

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4258,7 +4258,6 @@ router.post('/searchtimerecordemployee', async (req, res) => {
     if (year) query.year = { $regex: new RegExp(year, 'i') };
 
     if (!employeeId && !month && !year) {
-      console.log('No query parameters provided');
       return res.status(200).json({ result: [], message: 'No query parameters provided' });
     }
 
@@ -4295,7 +4294,6 @@ router.post('/searchtimerecordemployee', async (req, res) => {
           addSalaryList: calculatedValues.addSalaryList,
           sumCashWorkMul: calculatedValues.sumCashWorkMul,
         };
-        console.log('calculatedValues.addSalaryList ' + JSON.stringify(calculatedValues.addSalaryList[0],null,2))
         // ✅ Log BEFORE update
         // console.log(`🔍 BEFORE update (doc ${doc._id}):`, JSON.stringify(doc.addSalaryList, null, 2));
     
