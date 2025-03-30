f7edba0285827290a63023d0872b75110a4c4daf
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-30 08:36:02 +0700

    update account

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4294,7 +4294,8 @@ router.post('/searchtimerecordemployee', async (req, res) => {
           addSalaryList: calculatedValues.addSalaryList,
           sumCashWorkMul: calculatedValues.sumCashWorkMul,
         };
-        console.log('calculatedValues.addSalaryList ' + JSON.stringify(calculatedValues.addSalaryList.length,null,2))
+        // console.log('calculatedValues.addSalaryList ' + JSON.stringify(calculatedValues.addSalaryList.length,null,2))
+        console.log(calculatedValues.addSalaryList.length)
         // ✅ Log BEFORE update
         // console.log(`🔍 BEFORE update (doc ${doc._id}):`, JSON.stringify(doc.addSalaryList, null, 2));
     
