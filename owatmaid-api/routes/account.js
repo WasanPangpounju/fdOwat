b87f9d87e040efeb8e0fb7793931085a68f9f20a
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-31 08:56:00 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4518,7 +4518,7 @@ let check = false;
         item => String(item.id).trim() === cleanSalaryItemId
       );
 
-      if (existingItem ) {
+      if (existingItem && cleanSalaryItemId == item.id) {
         existingItem.SpSalary = parseFloat(existingItem.SpSalary  || 0) + amount;
         existingItem.message = parseFloat(existingItem.message  || 0) + 1;
 
