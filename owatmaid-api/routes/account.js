8de5ddf379dfe6e756a6cf1b98fc2e3b588a7a60
WasanPangpounju <friendlydev.net@gmail.com>
2025-03-31 08:22:01 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4505,7 +4505,7 @@ sumCashWorkMul[record?.cashOtMul ] += parseFloat(record?.cashBeforeOt || '0');
   if (record.addSalaryDaily && record.addSalaryDaily.length > 0) {
      addSalaryList = [];
 
-for (const record of employee_record) {
+for (const record of employee_record.addSalaryDaily) {
   if (Array.isArray(record.addSalaryDaily)) {
     record.addSalaryDaily.forEach((salaryItem) => {
       if (!salaryItem.id) return;
