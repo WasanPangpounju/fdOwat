d6e36fd036a844d4cd18b3f73f9a9bf7bf1d8345
WasanPangpounju <friendlydev.net@gmail.com>
2025-04-02 15:57:38 +0700

    test api accounting

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4424,7 +4424,8 @@ query.wGroup = await employeeProfile[0].department || '';
 // } 
 
 // console.log('workplaces?.[0]?.daysOff ' + JSON.stringify(workplaces?.[0]?.daysOff))
-console.log(new Date(workplaces?.[0]?.daysOff[2] ).toISOString().split('T')[0].toLocaleString("th-TH", { timeZone: "Asia/Bangkok" }) )
+let date = new Date(workplaces?.[0]?.daysOff[2] );
+console.log(date.toISOString().split('T')[0].toLocaleString("th-TH", { timeZone: "Asia/Bangkok" }) )
         } //end if
 
 } //end if
