aa91b654040462f9e7352dd9d81ececd1c94f2c5
WasanPangpounju <friendlydev.net@gmail.com>
2025-04-02 13:46:45 +0700

    test api accounting

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4409,7 +4409,7 @@ query.wGroup = await employeeProfile[0].department || '';
 
         // Query the workplace collection for matching documents
         const workplaces = await Workplace.find(query);
-        console.log('query ' + JSON.stringify(workplaces ) )
+        await console.log('query ' + JSON.stringify(workplaces ) )
 
         if(workplaces.length > 0 ) {
           console.log('workplaces.length ' + workplaces.length )
