2b5cf7ccfbb905930f4e0599a0123f7ec0184458
WasanPangpounju <friendlydev.net@gmail.com>
2025-04-29 03:26:56 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -4294,7 +4294,7 @@ router.post('/searchtimerecordbyworkplace', async (req, res) => {
 
             // 🔥 เพิ่มเช็ค dayWorkCount หรือ dayOffCount
             if (!record.dayWorkCount || !record.dayOffCount) {
-              console.log(`🔍 Missing dayWorkCount or dayOffCount for employeeId=${record.employeeId} month ${record.month} `);
+              console.log(`🔍 Missing dayWorkCount or dayOffCount for employeeId=${record.employeeId} month ${record.month} year ${record.myear}`);
       
               try {
                 const apiRes = await axios.post(sURL + '/conclude/searchtimerecordemployee', {
