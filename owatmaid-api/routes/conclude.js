c6d3717323c673e96ea31041b48d97b109b43b3c
WasanPangpounju <friendlydev.net@gmail.com>
2025-05-23 00:37:33 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1805,8 +1805,7 @@ const toBangkokDate = (input) => {
 };
 
 const checkDayRate = async (workplaceId, wGroup, date ) => {
-  // date  = await toBangkokDate(date);
-console.log("test" , workplaceId, wGroup, date );
+// console.log("test" , workplaceId, wGroup, date );
 // console.log(date.getDay() );
 
 //data for cal
