faf6f5f9114017ed69343f3ba75c3fa111aa2457
WasanPangpounju <friendlydev.net@gmail.com>
2025-05-22 23:35:55 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1832,7 +1832,7 @@ dataCal.holidayOT = await workplaces?.[0]?.holidayOT || 0;
 
 // console.log(JSON.stringify(workplaces?.[0]?.daysOff,null,2))
 for(let itemDay of workplaces?.[0]?.daysOff){
-  console.log(itemDay)
+  console.log(itemDay.toISOString().split('T')[0])
 }
 // dataCal?.daysOff
 const isDayOff = workplaces?.[0]?.daysOff?.some(d => 
