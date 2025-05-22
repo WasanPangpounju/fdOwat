8a642b01ee656cf62b6d4ae429a1b60eea756001
WasanPangpounju <friendlydev.net@gmail.com>
2025-05-23 00:51:04 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1848,6 +1848,7 @@ let isDayOff  = false;
 for(let itemDay of workplaces?.[0]?.daysOff){
   // console.log(itemDay.toISOString().split('T')[0])
   // console.log(date.toISOString().split('T')[0])
+  console.log(toBangkokDate(itemDay)  + ' = '+ date)
   if(toBangkokDate(itemDay) == date) {
   console.log(date.toLocaleString("th-TH", { timeZone: "Asia/Bangkok" }) )
 isDayOff   = true
