7b8a493f1ad57dd5e5929b160f7a45462a8867f3
WasanPangpounju <friendlydev.net@gmail.com>
2025-05-23 02:20:10 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1886,7 +1886,7 @@ for(const workTimeDay of workplaces[0].workTimeDay) {
 const dateObj = new Date(date); // สมมุติว่า parameter ชื่อ date
 const dayNumberx  = dateObj.getDay(); // 0 = อาทิตย์, 1 = จันทร์, ..., 6 = เสาร์
 if(dayNumber  > 20 ){
-console.log(dayNumberx  + ' ' + dateObj.getMonth() + ' = ' +  dayNumber )
+console.log(dayNumberx  + ' ' + dateObj.getMonth() + ' ' + dateObj.getFullYear() + ' = ' +  dayNumber )
 
 }
 
