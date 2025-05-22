305e45d7af3cf9f430bc769a02cdb258983a7b84
WasanPangpounju <friendlydev.net@gmail.com>
2025-05-23 03:08:30 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1883,12 +1883,19 @@ if(isDayOff == true) {
 for(const workTimeDay of workplaces[0].workTimeDay) {
   // let check = checkdayType(workTimeDay.startDay, workTimeDay.endDay , date.getDay());
   const [y, m, d] = date.split('-').map(Number);
-const dateObj = new Date(2025,3,23);
+const dateObj = new Date(y, m - 1, d);
 const dayNumberx = dateObj.getDay(); // 0 = อาทิตย์, ..., 6 = เสาร์
 const dateOfMonth = dateObj.getDate(); // 1 - 31
 
 if (dateOfMonth === 23) {
   console.log(dayNumberx + ' ' + dateObj.getMonth() + ' ' + dateObj.getFullYear() + ' = ' + dateOfMonth + ' ' + m);
+  const testDate = new Date('2025-03-23T00:00:00'); // ระบุเวลา 00:00:00 เพื่อเลี่ยงปัญหา Time Zone
+console.log('--- Test Date 2025-03-23 ---');
+console.log('getDay():', testDate.getDay()); // ควรได้ 0
+console.log('getMonth():', testDate.getMonth()); // ควรได้ 2
+console.log('getDate():', testDate.getDate()); // ควรได้ 23
+console.log('getFullYear():', testDate.getFullYear()); // ควรได้ 2025
+ 
 }
 
 
