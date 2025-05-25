622fd6f1f1b2077b9f11b918c249da827d7d4b78
WasanPangpounju <friendlydev.net@gmail.com>
2025-05-26 00:39:08 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1970,7 +1970,7 @@ const rawDate = new Date(year, month - 1, record.date); // สร้างวั
 const bangkokDate = toBangkokDate(rawDate); // ปรับให้ตรงกับเวลาไทย
 
 const dataRate = await checkDayRate(workplaceId, record.wGroup, bangkokDate , record.date );
-if(record.date == 31 ) {
+if(record.date == 20 ) {
   console.log(record.date + ' ' + dataRate?.dayType )
 }
 
