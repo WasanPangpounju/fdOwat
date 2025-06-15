b4702bdce7041c590aa1d56bfd52d96a5a11a3c8
WasanPangpounju <friendlydev.net@gmail.com>
2025-06-15 08:57:41 +0700

    test api conclude

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -2043,12 +2043,12 @@ let dayNumberx = await dateObj.getDay(); // 0 = อาทิตย์, ..., 6 = 
 let dateOfMonth = await dateObj.getDate(); // 1 - 31
 // console.log(dayNumberx )
 // test
-if(dateOfMonth == 'NaN' ) {
+// if(dateOfMonth == 'NaN' ) {
   console.log('* ' + dayNumberx )
 
   // console.log(dateString )
   // console.log('paddedMonth ' + paddedMonth )
-}
+// }
 
 let check = await checkdayType(workTimeDay.startDay, workTimeDay.endDay, dayNumberx );
 
