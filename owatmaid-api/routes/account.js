afe9067c19c59ea3663a5e6b326de9fc825f0e1f
Niceppn <PPhutana01@gmail.com>
2025-06-24 16:43:54 +0700

    update calculation

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -5099,7 +5099,7 @@ console.log(`💰 เงินสำหรับวันหยุดที่�
     if (costtype === "ภ.ง.ด.3") {
       socialSecurity = 0;
       // คำนวณภาษีหัก ณ ที่จ่าย รวมเงินพิเศษทุกประเภท
-      tax = Math.eil(
+      tax = Math.ceil(
         (parseFloat(sumCashWork || 0) + 
          parseFloat(sumCashOt || 0) + 
          parseFloat(addSalarySocialSecurity || 0) + 
