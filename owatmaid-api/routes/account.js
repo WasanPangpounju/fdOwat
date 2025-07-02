f631ae2219c276322f6ca981ed1edb9679f5537f
Niceppn <PPhutana01@gmail.com>
2025-07-02 16:14:19 +0700

    update Schema

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -5339,7 +5339,6 @@ console.log(`💰 เงินสำหรับวันหยุดที่�
   return await {
     dayWorkCount,
     dayOffCount,
-    specialDayOff,
     customizeDayoff, // เพิ่มฟิลด์ customizeDayoff
     cashcustomizeDayoff, // เพิ่มฟิลด์ cashcustomizeDayoff
     publicHolidayCount, // เพิ่มฟิลด์ publicHolidayCount
