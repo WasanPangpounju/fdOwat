dfab44818a9cb6733e18243fc30ab672bde9cec9
Niceppn <PPhutana01@gmail.com>
2025-07-09 15:18:07 +0700

    updateAccount

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -5087,7 +5087,7 @@ try {
   const sumCashSpecialDay = sumCashWork / dayWorkCount;
   const totalsumCashSpecialDay = sumCashSpecialDay * specialDay
 
-  cashSpecialDay = totalsumCashSpecialDay
+  
   specialDayOff = specialDay;
 
   console.log('cashSpecialDay  ' + cashSpecialDay);
