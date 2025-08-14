aea3848490d5be1daf4dbda4a995f859c1ba7bcf
Niceppn <PPhutana01@gmail.com>
2025-08-14 22:49:59 +0700

    update schema

--- owatmaid-api/routes/models/workplaceModel.js
+++ owatmaid-api/routes/models/workplaceModel.js
@@ -178,10 +178,8 @@ const workplaceSchema = new mongoose.Schema({
   },
   // เพิ่มใน workplaceSchema
 employeeCompensation: {
-  current: { type: Number, default: 0 }, // อัตราปัจจุบัน (%)
-  adjustment: { type: Number, default: 0 }, // ปรับเพิ่ม (%)
-  newRate: { type: Number, default: 0 }, // อัตราใหม่ (คำนวณอัตโนมัติ)
-  effectiveDate: { type: Date }, // วันที่เริ่มใช้
+  Rate21_30_31: { type: Number, default: 0 }, // เงินสงเคราะห์ลูกจ้าง วันที่ 21-30/31
+  Rate1_20: { type: Number, default: 0 }, // เงินสงเคราะห์ลูกจ้าง วันที่ 1-20
   history: [{
     oldRate: Number,
     newRate: Number,
