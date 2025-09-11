a5d185f91498e21de7734b6adcbc3c1da8a3627e
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-12 05:55:03 +0700

    add function getWeekendDatesEmployee

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -2996,7 +2996,7 @@ const calculateCashValuesSpecial7Days = async (employeeId, employee_record, mont
       
       // ตรวจสอบว่าพนักงานมาทำงานหรือไม่ (มีเวลาทำงาน > 0)
       const hasWorked = record.totalTime && parseFloat(record.totalTime) > 0;
-      console.log(' isHoliday '+ isHoliday + ' ' + JSON.stringify(allHolidays) );
+      console.log(' isHoliday '+ isHoliday + ' ' + JSON.stringify(isWeekendOrCustom ) );
 
       if (isHoliday && hasWorked) {
         // ถ้าเป็นวันหยุดและพนักงานมาทำงาน
