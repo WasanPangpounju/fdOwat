2cc20baaad570b2fec10239914d776b7c1693872
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-13 00:34:49 +0700

    add function getWeekendDatesEmployee

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -2919,7 +2919,7 @@ const calculateCashValuesSpecial7Days = async (employeeId, employee_record, mont
     const weekendResponse = await axios.get(apiUrl);
     weekendData = weekendResponse.data;
     // weekendData = await getWeekendDatesEmployee(year, month , existingEmployee[0].customWorkplace );
-console.log('ดึงข้อมูลวันหยุดจากการทำงานเฉพาะบุคคล')
+console.log('*ดึงข้อมูลวันหยุดจากการทำงานเฉพาะบุคคล')
 console.log('ข้อมูลหน่วยงานเฉพาะบุคคล ' + JSON.stringify(existingEmployee[0].customWorkplace ))
 //bb
     console.log(`📋  ข้อมูลวันหยุดที่ได้:`);
