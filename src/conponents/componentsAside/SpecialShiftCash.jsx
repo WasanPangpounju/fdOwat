0c7a94a8a195f196cf2f5fae2213c235fd69728b
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-13 13:13:10 +0700

    update conclude

--- src/conponents/componentsAside/SpecialShiftCash.jsx
+++ src/conponents/componentsAside/SpecialShiftCash.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { pdf } from '@react-pdf/renderer';
-import AllWorkplacesSummaryPDFReport from '../PDF/AllWorkplacesSummaryPDFReport';
+// import AllWorkplacesSummaryPDFReport from '../PDF/AllWorkplacesSummaryPDFReport';
 import Swal from 'sweetalert2';
 
 function SpecialShiftCash() {
